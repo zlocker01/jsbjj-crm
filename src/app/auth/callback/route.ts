354cdd918c5cce93c693693da170a748d285219b
Diego Beristain <beristaindelamora@gmail.com>
2025-06-21 00:37:31 -0600

    fix: :alien: fixing route into auth callback api
    /session/profile is the new auth confirmation url callback

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -5,7 +5,7 @@ export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get("code");
   // if "next" is in param, use it as the redirect URL
-  const next = searchParams.get("next") ?? "session/profile";
+  const next = searchParams.get("next") ?? "/session/profile";
 
   if (code) {
     const supabase = await createClient();
